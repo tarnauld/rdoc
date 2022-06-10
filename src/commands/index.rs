4460b44e43b4c9c3bc2e2c57940c2f7a90b51bcf
Timothée Arnauld <timotheearnauld@mbp-de-timothee.home>
2022-06-10 16:26:54 +0200

    :sparkles: add force option for index command

--- src/commands/index.rs
+++ src/commands/index.rs
@@ -1,16 +1,32 @@
 use crate::utils::file;
 use crate::utils::repo;
-use clap::Command;
+use clap::{Arg, Command};
+use std::path::Path;
 
 pub fn init_index_command<'index>() -> Command<'index> {
-    return Command::new("index").about("Generate the rdoc index.");
+    return Command::new("index")
+        .about("Generate the rdoc index.")
+        .arg(Arg::new("force").long("force").takes_value(false));
 }
 
-pub fn create_index() {
+pub fn create_index(force: bool) {
     let commits = repo::get_commits();
+
+    if check_if_index_exists() && !force {
+        println!(
+            "The index file already exists. 
+Try `rdoc update` if you want to update or `rdoc index --force` if you want to reset all changes.
+        "
+        );
+        return;
+    }
 
     match file::write_file(commits) {
         Ok(_) => (),
         Err(_) => println!("Rdoc is not installed. Please considering using `rdoc init` command"),
     };
+}
+
+fn check_if_index_exists() -> bool {
+    Path::new("./.rdoc/index.json").exists()
 }
--- src/matcher.rs
+++ src/matcher.rs
@@ -1,13 +1,15 @@
-use crate::commands::{index, init, tag, authors, generate, describe};
+use crate::commands::{authors, describe, generate, index, init, tag};
 use clap::ArgMatches;
 
 pub fn match_commands(matches: ArgMatches) {
     match matches.subcommand() {
         Some(("init", _)) => init::init_project(),
-        Some(("index", _)) => index::create_index(),
+        Some(("index", args)) => index::create_index(args.is_present("force")),
         Some(("tag", args)) => tag::create_tag(args.value_of("commit"), args.value_of("tag")),
         Some(("generate", _)) => generate::generate(),
-        Some(("authors", args)) => authors::update_authors(args.value_of("commit"), args.value_of("authors")),
+        Some(("authors", args)) => {
+            authors::update_authors(args.value_of("commit"), args.value_of("authors"))
+        }
         Some(("describe", args)) => describe::update_description(args.value_of("commit")),
         _ => std::process::exit(0),
     }
