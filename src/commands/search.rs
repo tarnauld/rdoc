24c96a959f9cb1c5ea5dd431ead11367aace0af7
Timothée Arnauld <timotheearnauld@mbp-de-timothee.home>
2022-06-13 11:38:23 +0200

    ✅ update search help integration tests

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -1,12 +1,17 @@
+use crate::utils::{file, repo};
 use clap::Arg;
 use clap::Command;
-use crate::utils::{repo, file};
 
 pub fn init_search_command<'search>() -> Command<'search> {
     return Command::new("search")
         .about("Returns a list of commits containing expression.")
         .arg(Arg::new("terms").required(true))
-        .arg(Arg::new("all").long("all").takes_value(false));
+        .arg(
+            Arg::new("all")
+                .long("all")
+                .takes_value(false)
+                .long_help("Search in commit message and description"),
+        );
 }
 
 pub fn search(terms_opt: Option<&str>, all_opt: bool) {
@@ -14,9 +19,8 @@ pub fn search(terms_opt: Option<&str>, all_opt: bool) {
 
     match all_opt {
         true => search_all(terms),
-        false => search_in_message(terms)
+        false => search_in_message(terms),
     }
-
 }
 
 fn search_in_message(terms: &str) {
@@ -30,7 +34,7 @@ fn search_in_message(terms: &str) {
 
 fn search_all(terms: &str) {
     let commits = file::get_commits_from_index();
-    
+
     commits.iter().for_each(|commit| {
         if commit.message.contains(terms) || commit.description.contains(terms) {
             println!("{}: {}", commit.id, commit.message);
